160b8289e956b71eebcdd4cbc9d9a151dfdd6d43
Abdias <abdiasejr@gmail.com>
2022-02-06 19:06:07 -0300

    team channel list component. cases when error, loading. team structure and direct messages structure

---
+++ client/src/components/TeamChannelList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { AddChannel } from "../assets";
+
+const TeamChannelList = ({ children, error = false, loading, type }) => {
+  if (error) {
+    return type === "team" ? (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message">
+          There was an error loading the team channels.
+        </p>
+      </div>
+    ) : null;
+  }
+
+  if (loading) {
+    return (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message loading">
+          {type === "team" ? "Channels" : "Messages"} loading...
+        </p>
+      </div>
+    );
+  }
+  
+  return (
+      <div className="team-channel-list">
+          <div className="team-channel-list__header">
+              <p className="team-channel-list__header__title">
+                  {type === 'team' ? 'Channels' : 'Direct Messages'}
+              </p>
+              {{/* Button - add channel */}}
+          </div>
+          {children}
+      </div>
+  )
+};
+
+export default TeamChannelList;
